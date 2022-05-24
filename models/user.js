e55753ea321657a319797dae5eb5fa63cdd49846
Sandhya <sandhyasenthakuri23@gmail.com>
2022-05-24 13:12:58 +0545

    commit -m

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
-const mongoose = require("mongoose"),
-  passportLocalMongoose = require("passport-local-mongoose");
-
+const mongoose = require("mongoose");
+const bcrypt=require('bcryptjs')
+const validator = require('validator')
+const jwt=require('jsonwebtoken')
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
@@ -20,7 +21,13 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
       trim: true,
-      required:true,
+      required: true,
+      unique:[true,"Email address is already taken."],
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is not valid")
+      }
+    }
     },
    phoneno: {
         type: String,
@@ -29,20 +36,66 @@ const userSchema = new mongoose.Schema({
       },
     password: {
         type: String,
-       required:true,
-      },
-      
-    createdAt: { type: Date, default: Date.now() },
-    updatedAt:{ type:Date},
+      required: true,
+      minlength:[6,"Password must be minimum 6 characters"]
+  },
+  tokens:[{
+    token:{
+        type:String,
+        required:true,
+        trim:true
+        },
+},
+],  
   gender: String,
   address: String,
   image: {
     type: String,
     default: "",
   },
-  isAdmin: { type: Boolean, default: false },
+  role:{
+    type: String,
+    required: true,
+    enum: ["admin", "basic"],
+    default:"basic"
+  }
+}, {
+  timestamps: true
 });
+userSchema.method.toJSON=function(){
+  const user=this;
+  const userObject=user.toObject();
+  delete userObject.password;
+  delete userObject.token;
+  return userObject;
+};
+userSchema.methods.generateAuthToken=async function(){
 
-userSchema.plugin(passportLocalMongoose);
+  const user=this;
+  const token=jwt.sign({_id:user._id},process.env.JWT_SECRET)
+  user.tokens=user.tokens.concat({token});
+  await user.save();
+  return token;
+};
+userSchema.statics.findByCredentials = async (email,password)=>{
+  const user=await User.findOne({email});
+  if(!user){
+      throw new Error("User doesnot exist");
+  }
+  const isMatch= await bcrypt.compare(password,user.password);
+  if(!isMatch){
+      throw new Error("Username and password doesnot match");
+  }
+  return user;
+};
+userSchema.pre("save",async function(next){
+  const user=this;
+  if(user.isModified("password")){
+      user.password=await bcrypt.hash(user.password,8);
+  }
+  next();
+});
+const User=mongoose.model("User",userSchema);
+module.exports=User;
 
 module.exports = mongoose.model("User", userSchema);
